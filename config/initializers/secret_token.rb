8c5a46e990156ce872bdef1f46e0e7ec9ce812cc
Brad Patton <bradpatton3.14@gmail.com>
2013-03-10 20:05:23 -0500

    original

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ftz::Application.config.secret_token = '890d5882f640f825f1b1710f06a0b5dfcaa057537e67d75b87f511e56889177b365327f26d79367251070c2305194d1e0be590ac8fbc97cc95c317c7ec978037'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ftz::Application.config.session_store :cookie_store, key: '_ftz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ftz::Application.config.session_store :active_record_store
